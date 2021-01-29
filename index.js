2c1184b5c0d130774c74f485106066b9fd4a838b
Hugh Rundle <hugh@hughrundle.net>
2021-01-29 16:51:23 +1100

    prepare action for prod

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-
-core.info(`GitHub event name is ${process.env.GITHUB_EVENT_NAME}`)
 core.info(`github Actor ${process.env.GITHUB_ACTOR}`)
 core.info(`github HEAD ref is ${process.env.GITHUB_HEAD_REF}`)
-core.info(`github BASE ref is ${process.env.GITHUB_BASE_REF}`)
-// if ( LOGIC HERE) {
-//   core.setFailed('Does not qualify to be auto-merged')
-// }
+
+const actor = process.env.GITHUB_ACTOR;
+const head = process.env.GITHUB_HEAD_REF;
+const requiredUser = core.getInput('required-user');
+const requiredBranch = core.getInput('required-branch');
+const requireBoth = requiredUser != null && requiredBranch != null 
+actorMatches = requiredUser == actor;
+headMatches = requiredBranch == head;
+ok = requireBoth ? actorMatches && headMatches : requiredUser ? requiredUser == actor : requiredBranch ? requiredBranch == head : true;
+
+message = requireBoth ? `Only PRs from user ${requiredUser} on branch ${requiredBranch} can be auto-merged` : 
+          requiredUser ? `Only PRs from user ${requiredUser} can be auto-merged` : 
+          requiredBranch ? `Only PRs on branch ${requiredBranch} can be auto-merged` : 
+          `Something went wrong`;
+
+if ( !ok) {
+  core.setFailed(message)
+}
